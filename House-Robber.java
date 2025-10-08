e30dc354807e6e82b765a7b71cbdf925d4d1efde
Vinoth <131464734+mr-tamil-coder@users.noreply.github.com>
2025-10-08 23:51:45 +0530

    [198] [Time Beats: 100.00%] [Memory Beats: 8.69%] - LeetPush

--- House-Robber.java
+++ House-Robber.java
@@ -4,12 +4,13 @@ class Solution {
         if(n == 1){
             return nums[0];
         }
-        int dp[]= new int[n];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
+       int p2 = nums[0];
+        int p1 = Math.max(nums[0],nums[1]);
         for(int i=2;i<n;i++){
-            dp[i] = Math.max(dp[i-2] + nums[i],dp[i-1]);
+            int current = Math.max(p2 + nums[i],p1);
+            p2 = p1;
+            p1 = current;
         }
-        return dp[n-1];
+        return p1;
     }
 }
